28dbff4a42c096a8cc96aede3208bf6f7e9f26ab
Sakshi <saks0705@gmail.com>
2025-09-16 18:46:00 +0530

    binary-search

---
+++ dsa/3BinarySearch/BinarySearchCode/BinarySearch.java
@@ -0,0 +1,24 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = { 1, 3, 26, 37, 47, 55, 56, 72, 88 };
+        System.out.println("index of 56 is " + search(arr, 72));
+    }
+
+    static int search(int[] arr, int target) {
+        int start = 0, end = arr.length - 1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+}
---
+++ dsa/3BinarySearch/BinarySearchCode/OrderAgnostic.java
@@ -0,0 +1,35 @@
+public class OrderAgnostic {
+    public static void main(String[] args) {
+        int[] arr = { 1, 3, 26, 37, 47, 55, 56, 72, 88 };
+        boolean isAscending = arr[0] < arr[arr.length - 1];
+        System.out.println("index of 56 is " + agnosticSearch(arr, 72,isAscending));
+    }
+
+    static int agnosticSearch(int[] arr, int target, boolean isAscending) {
+        int start = 0, end = arr.length - 1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            }
+            if (isAscending) {
+                if (arr[mid] > target) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (arr[mid] > target) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+
+        }
+
+        return -1;
+    }
+}
